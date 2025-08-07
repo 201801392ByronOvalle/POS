6acba3e634c8322a19840265911b7e99b2a74da4
Byron Ovalle <anovalle@farmaciasgaleno.com.gt>
2025-08-07 12:44:37 -0600

    feat: aprendiendo sobre los controladores y controllers. Reto resuelto.

--- posnest/src/app.controller.ts
+++ posnest/src/app.controller.ts
@@ -4,9 +4,29 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  //Para generar un controlador en CLI
+  /* 
+    nest generate controller nombre_controlador
+    nest g co nombre_controlador
+  */
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  getPost() {
+    return "Desde @Post"
+  }
+
+  //reto
+  @Put()
+  getPut() {
+    return "Desde @Put"
+  }
+
+  @Patch()
+  getPatch() {
+    return "Desde @Patch"
   }
 }
