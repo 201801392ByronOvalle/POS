ed01b0f8ed560b0eed4cdcb4443ffef48bf0c308
Byron Ovalle <anovalle@farmaciasgaleno.com.gt>
2025-08-08 11:48:50 -0600

    feat: conociendo los dto

--- posnest/src/categories/dto/create-category.dto.ts
+++ posnest/src/categories/dto/create-category.dto.ts
@@ -1,3 +1,6 @@
+import { IsNotEmpty } from 'class-validator'
+
 export class CreateCategoryDto {
+    @IsNotEmpty({message: 'El Nombre de la Categoría no puede ir vacío'}) //En el decorador @Decorador se usa ya fecta a la sig. linea de codigo
     name: string
 }
