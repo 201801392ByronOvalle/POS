034a15260a6f73b76c999d29145a673dc5dab292
Byron Ovalle <anovalle@farmaciasgaleno.com.gt>
2025-08-08 15:37:56 -0600

    feat: conectando la DB a typeorm

---
+++ posnest/src/config/typeorm.config.ts
@@ -0,0 +1,15 @@
+import { ConfigService } from '@nestjs/config'
+import {  TypeOrmModuleOptions } from '@nestjs/typeorm'
+
+export const typeOrmConfig = (configService : ConfigService) : TypeOrmModuleOptions   => ({
+    type: 'mssql',
+    host: configService.get('DATABASE_HOST'),
+    //port: +configService.get('DATABASE_PORT'), //Para SQL Server parsear a numero con + ya que siempre envia como string
+    username: configService.get('DATABASE_USER'),
+    password: configService.get('DATABASE_PASS'),
+    database: configService.get('DATABASE_NAME'),
+    options: {
+        trustServerCertificate: true,
+    },
+    logging: true
+})
