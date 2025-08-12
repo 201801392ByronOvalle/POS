72caf9815570cc1bfd5fa1f2812fb208ff97d3dd
Byron Ovalle <anovalle@farmaciasgaleno.com.gt>
2025-08-12 08:19:43 -0600

    feat: obtener todos los Productos

--- posnest/src/products/dto/create-product.dto.ts
+++ posnest/src/products/dto/create-product.dto.ts
@@ -1,1 +1,19 @@
-export class CreateProductDto {}
+import { IsInt, IsNotEmpty, IsNumber, IsString } from "class-validator"
+
+export class CreateProductDto {
+    @IsNotEmpty({message: 'El Nombre del Producto es Obligatorio'})
+    @IsString({message: 'Nombre no válido'})
+    name: string
+    
+    @IsNotEmpty({message: 'El Precio del Producto es Obligatorio'})
+    @IsNumber({maxDecimalPlaces: 2} ,{message: 'Precio no válido'})
+    price: number
+
+    @IsNotEmpty({message: 'La cantidad no puede ir vacía'})
+    @IsNumber({maxDecimalPlaces: 0} ,{message: 'Cantidad no válida'})
+    inventory: number
+    
+    @IsNotEmpty({message: 'La categoría es obligatoria'})
+    @IsInt({message: 'La categoría no es valida'})
+    categoryId: number
+}
